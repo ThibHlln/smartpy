7586c07f475365532dbe60c4d039ce97c243d7d0
ThibHlln <thibault.hallouin@ucdconnect.ie>
2018-01-29 17:04:19 +0000

    adds gw_constraint obj function
    returns a value of 0 or 1 depending on if the value for simulation is
    between +/- 0.1 of the groundwater coef (evaluation data)
    
    [also renamed the function for nash sutcliffe]

--- SMARTobjective.py
+++ SMARTobjective.py
@@ -6,13 +6,15 @@ def calculate_obj_fn(simulation, evaluation, method):
     if not len(simulation) == len(evaluation):
         raise Exception("Simulation and Evaluation series have different length.")
     if method == 'nse':
-        return calculate_nse(simulation, evaluation)
+        return nash_sutcliffe(evaluation, simulation)
+    elif method == '':
+        return groundwater_constraint(evaluation, simulation)
 
 
-def calculate_nse(flows_mod, flows_obs):
+def nash_sutcliffe(evaluation, simulation):
 
     # Remove the steps that are missing observations
-    nda_flows_mod, nda_flows_obs = numpy.asarray(flows_mod), numpy.asarray(flows_obs)
+    nda_flows_mod, nda_flows_obs = numpy.asarray(simulation), numpy.asarray(evaluation)
 
     # calculate mean of observations
     mean_flow_obs = numpy.mean(nda_flows_obs)
@@ -21,3 +23,10 @@ def calculate_nse(flows_mod, flows_obs):
     nse = 1 - (numpy.sum((nda_flows_obs - nda_flows_mod) ** 2) / numpy.sum((nda_flows_obs - mean_flow_obs) ** 2))
 
     return nse
+
+
+def groundwater_constraint(evaluation, simulation):
+    if (evaluation[0] - 0.1 <= simulation[0]) or (simulation[0] <= evaluation[0] + 0.1):
+        return 1.0
+    else:
+        return 0.0
