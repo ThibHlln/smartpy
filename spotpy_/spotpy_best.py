d54d367547806fb017fb3fb7457f06b0fc8970e7
ThibHlln <thibault.hallouin@ucdconnect.ie>
2018-03-29 18:12:23 +0100

    adds functionality to run N best sets in spotpy
    similar to GLUE script, it selects the N (user defined) best parameters on an objective function and uses spotpy to run simulation on evaluation period using these best sets

---
+++ spotpy_/spotpy_best.py
@@ -0,0 +1,217 @@
+import argparse
+from csv import DictReader
+from itertools import izip
+from os import path, getcwd, sep
+
+import numpy as np
+import spotpy
+
+from scripts.SMARTfiles import get_dict_simulation_settings
+from scripts.SMARTobjective import groundwater_constraint, bounded_nash_sutcliffe
+from scripts.SMARTpy import SMART
+
+
+class SpotPySetUp(object):
+    def __init__(self, catchment, nb_best, root_f, save_sim=False):
+        in_f = sep.join([root_f, 'in', catchment, sep])
+
+        c_area, g_area, start, end, delta_simu, delta_report, warm_up, gw_constraint = \
+            get_dict_simulation_settings(''.join([in_f, catchment, '.sttngs']))
+
+        self.model = SMART(catchment, c_area, g_area, start, end, delta_simu, delta_report, warm_up, root_f)
+
+        self.save_sim = save_sim
+
+        self.constraints = {'gw': gw_constraint}
+
+        # extract behavioural sets from sampling sets
+        self.sampling_run_file = ''.join([in_f, catchment, '_LHS_SMART.csv'])
+        self.sampled_params, self.sampled_obj_fns = get_sampled_sets_from_file(self.sampling_run_file)
+        self.constraints_values = [(1.0,)]
+        self.constraints_types = ['equal']
+        self.best_params = get_best_sets(self.sampled_params, self.sampled_obj_fns[:, [6]],
+                                         self.constraints_values, self.constraints_types,
+                                         self.sampled_obj_fns[:, [0]], nb_best)
+        # give list of behavioural parameters
+        self.param_names = ['T', 'C', 'H', 'D', 'S', 'Z', 'SK', 'FK', 'GK', 'RK']
+        self.params = [
+            spotpy.parameter.List(self.param_names[0], self.best_params[:, 0]),
+            spotpy.parameter.List(self.param_names[1], self.best_params[:, 1]),
+            spotpy.parameter.List(self.param_names[2], self.best_params[:, 2]),
+            spotpy.parameter.List(self.param_names[3], self.best_params[:, 3]),
+            spotpy.parameter.List(self.param_names[4], self.best_params[:, 4]),
+            spotpy.parameter.List(self.param_names[5], self.best_params[:, 5]),
+            spotpy.parameter.List(self.param_names[6], self.best_params[:, 6]),
+            spotpy.parameter.List(self.param_names[7], self.best_params[:, 7]),
+            spotpy.parameter.List(self.param_names[8], self.best_params[:, 8]),
+            spotpy.parameter.List(self.param_names[9], self.best_params[:, 9])
+        ]
+        # set up a database to custom save results
+        self.database = file(self.model.out_f + '{}_{}BEST_SMART.csv'.format(catchment, nb_best), 'wb')
+        self.simu_steps = [dt.strftime("%Y-%m-%d %H:%M:%S") for dt in self.model.flow.iterkeys()] \
+            if self.save_sim else []
+        self.obj_fn_names = ['NSE', 'logNSE', 'Bias', 'KGE', 'RMSE', 'C2M'] if self.constraints['gw'] == -999.0 \
+            else ['NSE', 'logNSE', 'Bias', 'KGE', 'RMSE', 'C2M', 'GW']
+        # write header in database file
+        self.database.write(','.join(self.obj_fn_names + self.param_names + self.simu_steps) + '\n')
+
+    def parameters(self):
+        # returns an ndarray containing a tuple for each parameter (random value, step, optguess, minbound, maxbound)
+        # parameter.generate() loops through each Object in self.params
+        # and uses its astuple() method (inherited from the Base class)
+        # that itself uses its __call__() (also inherited from the Base class)
+        return spotpy.parameter.generate(self.params)
+
+    def simulation(self, vector):
+        simulations, constraint = self.model.simulate({
+            'T': vector[0], 'C': vector[1], 'H': vector[2], 'D': vector[3], 'S': vector[4], 'Z': vector[5],
+            'SK': vector[6], 'FK': vector[7], 'GK': vector[8], 'RK': vector[9]
+        })
+        # returns only simulations with observations
+        return [[simulations[dt] for dt in self.model.flow.iterkeys()], [constraint]]
+
+    def evaluation(self):
+        return [[observation for observation in self.model.flow.itervalues()], [self.constraints['gw']]]
+
+    def objectivefunction(self, simulation, evaluation):
+        obj1 = spotpy.objectivefunctions.nashsutcliffe(evaluation=evaluation[0], simulation=simulation[0])
+        obj2 = spotpy.objectivefunctions.lognashsutcliffe(evaluation=evaluation[0], simulation=simulation[0])
+        obj3 = spotpy.objectivefunctions.bias(evaluation=evaluation[0], simulation=simulation[0])
+        obj4 = spotpy.objectivefunctions.kge(evaluation=evaluation[0], simulation=simulation[0])
+        obj5 = spotpy.objectivefunctions.rmse(evaluation=evaluation[0], simulation=simulation[0])
+        obj6 = bounded_nash_sutcliffe(evaluation=evaluation[0], simulation=simulation[0])
+        obj7 = groundwater_constraint(evaluation=evaluation[1], simulation=simulation[1])
+        if self.constraints['gw'] == -999.0:
+            return [obj1, obj2, obj3, obj4, obj5, obj6]
+        else:
+            return [obj1, obj2, obj3, obj4, obj5, obj6, obj7]
+
+    def save(self, obj_fns, parameters, simulations, *args, **kwargs):
+        if self.save_sim:
+            line = ','.join([
+                ','.join('%e' % obj_fn for obj_fn in obj_fns),
+                ','.join('%e' % parameter for parameter in parameters),
+                ','.join('%e' % simulation for simulation in simulations[0])]) + '\n'
+        else:
+            line = ','.join([
+                ','.join('%e' % obj_fn for obj_fn in obj_fns),
+                ','.join('%e' % parameter for parameter in parameters)]) + '\n'
+        self.database.write(line)
+
+
+def get_sampled_sets_from_file(file_location):
+    with open(file_location) as my_file:
+        my_reader = DictReader(my_file)
+        obj_fns, params = list(), list()
+        for row in my_reader:
+            obj_fns.append([row['like1'], row['like2'], row['like3'], row['like4'],
+                            row['like5'], row['like6'], row['like7']])
+            params.append([row['parT'], row['parC'], row['parH'], row['parD'], row['parS'],
+                           row['parZ'], row['parSK'], row['parFK'], row['parGK'], row['parRK']])
+
+    return np.array(params, dtype=np.float64), np.array(obj_fns, dtype=np.float64)
+
+
+def get_best_sets(params, constraints_fns, constraints_val, constraints_typ, sort_fn, nb_best):
+
+    if constraints_fns.ndim != 2:
+        raise Exception('The matrix containing the constraint functions is not 2D.')
+    if params.ndim != 2:
+        raise Exception('The matrix containing the parameters is not 2D.')
+    if constraints_fns.shape[0] != params.shape[0]:
+        raise Exception('The matrices containing constraint functions and parameters have different sample sizes.')
+    if not ((constraints_fns.shape[1] == len(constraints_val)) and (constraints_fns.shape[1] == len(constraints_typ))):
+        raise Exception('The constraint function matrix and the conditions matrices '
+                        'do not have compatible dimensions.')
+
+    if sort_fn.shape[0] != params.shape[0]:
+        raise Exception('The matrices containing objective functions and parameters have different sample sizes.')
+    if nb_best > params.shape[0]:
+        raise Exception('The number of best models requested is higher than the sample size.')
+
+    constrained = np.ones((constraints_fns.shape[0],), dtype=bool)
+    for obj_fn, values, kind in izip(constraints_fns.T, constraints_val, constraints_typ):
+        if kind == 'equal':
+            if len(values) == 1:
+                selection = obj_fn == values[0]
+            else:
+                raise Exception("The tuple for \"equal\" condition does not contain one and only one element.")
+        elif kind == 'min':
+            if len(values) == 1:
+                selection = obj_fn >= values[0]
+            else:
+                raise Exception("The tuple for \"min\" condition does not contain one and only one element.")
+        elif kind == 'max':
+            if len(values) == 1:
+                selection = obj_fn <= values[0]
+            else:
+                raise Exception("The tuple for \"max\" condition does not contain one and only one element.")
+        elif kind == 'inside':
+            if len(values) == 2:
+                if values[1] > values[0]:
+                    selection = (obj_fn >= values[0]) & (obj_fn <= values[1])
+                else:
+                    raise Exception("The two elements of the tuple for \"inside\" are inconsistent.")
+            else:
+                raise Exception("The tuple for \"inside\" condition does not contain two and only two elements.")
+        elif kind == 'outside':
+            if len(values) == 2:
+                if values[1] > values[0]:
+                    selection = (obj_fn <= values[0]) & (obj_fn >= values[1])
+                else:
+                    raise Exception("The two elements of the tuple for \"outside\" are inconsistent.")
+            else:
+                raise Exception("The tuple for \"outside\" condition does not contain two and only two elements.")
+        else:
+            raise Exception("The type of threshold \"{}\" is not in the database.".format(kind))
+
+        constrained *= selection
+
+    sort_fn_constrained = sort_fn[constrained, :]
+    param_constrained = params[constrained, :]
+
+    if nb_best > param_constrained.shape[0]:
+        raise Exception('The number of best models requested is higher than the restrained sample size.')
+
+    return param_constrained[sort_fn_constrained[:, 0].argsort()][-nb_best:]
+
+
+def spotpy_instructions(catchment, nb_best, parallel, root_f):
+
+    spotpy_setup = SpotPySetUp(catchment, nb_best, root_f, save_sim=True)
+
+    sampler = spotpy.algorithms.mc(spotpy_setup, parallel=parallel)
+
+    sampler.sample(spotpy_setup.best_params.shape[0])
+
+
+if __name__ == '__main__':
+    # Define the root of the SMARTpy package
+    if getcwd() == path.dirname(path.realpath(__file__)):  # execution from the directory where the script is
+        smart_root = path.realpath('../..')  # move to parent of parent directory of this current python file
+    else:  # execution not from the directory where the script is
+        smart_root = getcwd()  # keep the current working directory
+
+    # Collect the arguments to set up SPOTPY
+    parser = argparse.ArgumentParser(description="simulate lumped catchment hydrology"
+                                                 "for one catchment and one time period"
+                                                 "using SPOTPY")
+    parser.add_argument('catchment', type=str,
+                        help="name of the catchment")
+    parser.add_argument('nb_best', type=int,
+                        help="size of the set of best performing models")
+    parser.add_argument('-s', '--sequence', dest='parallelisation', action='store_false',
+                        help="compute each sample in sequence ")
+    parser.add_argument('-p', '--parallel', dest='parallelisation', action='store_true',
+                        help="parallel computing of the sample  ")
+    parser.set_defaults(parallelisation=False)
+    args_ = parser.parse_args()
+
+    # send the relevant argument for parallelisation option
+    if args_.parallelisation:
+        parallelisation = 'mpi'  # use MPI and parallel computing
+    else:
+        parallelisation = 'seq'  # use traditional sequential computing
+
+    # Call main function containing SPOTPY instructions
+    spotpy_instructions(args_.catchment, args_.nb_best, parallelisation, smart_root)
